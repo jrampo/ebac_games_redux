d009b30fff5e0a46ff8e2d0a7c0a984915d8fb8d
Joao Rampo <joao.rampoo@gmail.com>
2024-09-10 17:55:42 -0300

    before aftereach after all

---
+++ src/containers/Produtos/__tests__/Produtos.test.tsx
@@ -0,0 +1,64 @@
+import { rest } from 'msw'
+import {setupServer} from 'msw/node'
+
+import Produtos from "..";
+import { renderizaComProvider } from "../../../utils/tests";
+
+const mocks = [
+    {
+        id: 1,
+        categoria: 'rpg',
+        imagem: '',
+        plataformas: ['windows'],
+        preco: 150.9,
+        precoAntigo: 199.9,
+        titulo: 'elden ring'
+      },
+      {
+        id: 2,
+        categoria: 'rpg',
+        imagem: '',
+        plataformas: ['windows', 'ps5', 'xboxS'],
+        preco: 199.9,
+        precoAntigo: 299.9,
+        titulo: 'hogwarts legacy'
+      },
+      {
+        id: 3,
+        categoria: 'ação',
+        imagem: '',
+        plataformas: ['ps5', 'xboxS'],
+        preco: 150,
+        precoAntigo: 200,
+        titulo: 'gotham knights'
+      },
+      {
+        id: 4,
+        categoria: 'aventura',
+        imagem: '',
+        plataformas: ['nintendo switch'],
+        preco: 189.9,
+        precoAntigo: 299.9,
+        titulo: 'donkey kong'
+      },
+
+]
+
+const server = setupServer(
+  rest.get('http://localhost:4000/produtos', 
+    (requisicao, resposta, contexto) => {
+      return resposta(contexto.json(mocks))
+  })
+)
+
+describe('testes para o container produtos', () => {
+  beforeAll(() => (server.listen()))
+  afterEach(() => server.resetHandlers())
+  afterAll(() => server.close())
+
+    test('deve renderizar corretamente', () => {
+        const { debug } = renderizaComProvider(<Produtos/>)
+        debug()
+    })
+})
+
